ea833fe66f206481c242d1f2008a09e6ca4ca494
Arsen Girinov <amateurlib@yandex.ru>
2021-03-16 21:54:24 +0300

    Initial commit

---
+++ src/test/java/com/github/arsengir/AppTest.java
@@ -0,0 +1,14 @@
+package com.github.arsengir;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    void main() {
+        App app = new App();
+        assertNotNull(app);
+    }
+}
